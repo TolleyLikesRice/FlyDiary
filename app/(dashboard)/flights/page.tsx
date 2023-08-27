1afbd7eea878a2c14ff70197296bd4b588104319
TolleyLikesRice <tolley@tolley.dev>
2023-08-27 14:13:48 +0100

    Flight page touchups

--- app/(dashboard)/flights/page.tsx
+++ app/(dashboard)/flights/page.tsx
@@ -3,9 +3,10 @@ import DatabaseProvider from '@/components/databaseProvider'
 
 import FlightTableSkeleton from './FlightTableSkeleton'
 
-export default function DashboardPage() {
+export default function FlightsPage() {
     return (
-        <div className='container mx-auto md:p-8 lg:p-16'>
+        <div className='container mx-auto md:p-8 lg:p-16 lg:py-8'>
+            <h1 className='text-white text-3xl pb-6'>All Flights</h1>
             <DatabaseProvider loadingSkeleton={<FlightTableSkeleton />} Element={FlightTable} />
         </div>
     )
